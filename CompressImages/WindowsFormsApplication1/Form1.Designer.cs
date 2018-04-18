fb35f537cbdd9b09d3b458ee4401c7f2269b7bd6
Phuc Nguyen <phucsecond6554@gmail.com>
2018-04-18 15:45:51 +0700

    Lam lai chuong trinh compress anh C#

---
+++ CompressImages/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtQualify = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCompress = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.la_outpath = new System.Windows.Forms.Label();
+            this.btnOuput = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(105, 4);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 70);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(813, 361);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 437);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(814, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(195, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ban da chon: ";
+            // 
+            // txtQualify
+            // 
+            this.txtQualify.Location = new System.Drawing.Point(106, 40);
+            this.txtQualify.Name = "txtQualify";
+            this.txtQualify.Size = new System.Drawing.Size(228, 20);
+            this.txtQualify.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Chon chat luong";
+            // 
+            // btnCompress
+            // 
+            this.btnCompress.Location = new System.Drawing.Point(751, 36);
+            this.btnCompress.Name = "btnCompress";
+            this.btnCompress.Size = new System.Drawing.Size(75, 23);
+            this.btnCompress.TabIndex = 8;
+            this.btnCompress.Text = "Compress";
+            this.btnCompress.UseVisualStyleBackColor = true;
+            this.btnCompress.Click += new System.EventHandler(this.btnCompress_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Input";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(344, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Output";
+            // 
+            // la_outpath
+            // 
+            this.la_outpath.AutoSize = true;
+            this.la_outpath.Location = new System.Drawing.Point(525, 9);
+            this.la_outpath.Name = "la_outpath";
+            this.la_outpath.Size = new System.Drawing.Size(74, 13);
+            this.la_outpath.TabIndex = 11;
+            this.la_outpath.Text = "Ban da chon: ";
+            // 
+            // btnOuput
+            // 
+            this.btnOuput.Location = new System.Drawing.Point(423, 4);
+            this.btnOuput.Name = "btnOuput";
+            this.btnOuput.Size = new System.Drawing.Size(75, 23);
+            this.btnOuput.TabIndex = 10;
+            this.btnOuput.Text = "Browse";
+            this.btnOuput.UseVisualStyleBackColor = true;
+            this.btnOuput.Click += new System.EventHandler(this.btnOuput_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 464);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.la_outpath);
+            this.Controls.Add(this.btnOuput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCompress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtQualify);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtQualify;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCompress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label la_outpath;
+        private System.Windows.Forms.Button btnOuput;
+    }
+}
+
---
+++ CompressImages/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+//using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Threading;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private string[] _imagePaths;
+        private string _outpath = "Output/";
+        private int _qualify = 50;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static ImageCodecInfo GetEncoderInfo(string mimeType)
+        {
+            // Get image codecs for all image formats 
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+
+            // Find the correct image codec 
+            for (int i = 0; i < codecs.Length; i++)
+                if (codecs[i].MimeType == mimeType)
+                    return codecs[i];
+
+            return null;
+        }
+
+        public static void SaveJpeg(string path, Image image, int quality)
+        {
+            //ensure the quality is within the correct range
+            if ((quality < 0) || (quality > 100))
+            {
+                //create the error message
+                string error = string.Format("Jpeg image quality must be between 0 and 100, with 100 being the highest quality.  A value of {0} was specified.", quality);
+                //throw a helpful exception
+                throw new ArgumentOutOfRangeException(error);
+            }
+
+            //create an encoder parameter for the image quality
+            EncoderParameter qualityParam = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+            //get the jpeg codec
+            ImageCodecInfo jpegCodec = GetEncoderInfo("image/jpeg");
+
+            //create a collection of all parameters that we will pass to the encoder
+            EncoderParameters encoderParams = new EncoderParameters(1);
+            //set the quality parameter for the codec
+            encoderParams.Param[0] = qualityParam;
+            //save the image using the codec and the parameters
+            image.Save(path, jpegCodec, encoderParams);
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = true;
+
+            if (!Directory.Exists("Output"))
+            {
+                Directory.CreateDirectory("Output");
+            }
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                this._imagePaths = dialog.FileNames;
+                
+                label1.Text += _imagePaths.Length.ToString() + " files";
+            }
+        }
+
+        private void btnCompress_Click(object sender, EventArgs e)
+        {
+            int files = _imagePaths.Length;
+            this._qualify = Int32.Parse(txtQualify.Text);
+
+            //int count = 0;
+            foreach (var path in _imagePaths)
+            {
+                Image image = Image.FromFile(path);
+
+                pictureBox1.Image = image;
+                pictureBox1.Refresh();
+
+                string outpath = _outpath + "/" + Path.GetFileName(path);
+                SaveJpeg(outpath, image, this._qualify);
+                image.Dispose();
+
+                progressBar1.Minimum = 0;
+                progressBar1.Maximum = files;
+
+                progressBar1.Value += 1;
+
+                Thread.Sleep(1000);
+            }
+
+            MessageBox.Show("Done");
+        }
+
+        private void btnOuput_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                _outpath = dialog.SelectedPath;
+
+                la_outpath.Text = _outpath;
+            }
+
+        }
+    }
+}
